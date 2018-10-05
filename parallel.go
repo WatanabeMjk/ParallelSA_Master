0b48e7ca4d96b13c68a34c12d8c2832e75dcf1c6
WatanabeMjk <nonpedia@gmail.com>
2018-10-05 20:06:53 +0900

    Sample_push

--- parallel.go
+++ parallel.go
@@ -1,49 +1,16 @@
 package main
 
 import (
+    "fmt"
     "log"
     "time"
+    "math/rand"
 )
 
 func main() {
     log.Print("started.")
 
-    finished := make(chan bool)
-
-    // 配列
-    funcs := []func(){
-        func() {
-            // 1秒かかるコマンド
-            log.Print("sleep1 started.")
-            time.Sleep(1 * time.Second)
-            log.Print("sleep1 finished.")
-            finished <- true
-        },
-        func() {
-            // 2秒かかるコマンド
-            log.Print("sleep2 started.")
-            time.Sleep(2 * time.Second)
-            log.Print("sleep2 finished.")
-            finished <- true
-        },
-        func() {
-            // 3秒かかるコマンド
-            log.Print("sleep3 started.")
-            time.Sleep(3 * time.Second)
-            log.Print("sleep3 finished.")
-            finished <- true
-        },
-    }
-
-    // 並行化する
-    for _, sleep := range funcs {
-        go sleep()
-    }
-
-    // 終わるまで待つ
-    for i := 0; i < len(funcs); i++ {
-        <-finished
-    }
-
-    log.Print("all finished.")
+    rand.Seed(time.Now().UnixNano())
+    fmt.Println("My favorite number is", rand.Intn(10))
+    log.Print("end.")
 }
