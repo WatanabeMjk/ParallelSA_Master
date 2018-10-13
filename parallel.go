94463ba1dd8db2acf12f6a247e9fa32740c6ad68
WatanabeMjk <nonpedia@gmail.com>
2018-10-13 11:55:02 +0900

    CGO

--- parallel.go
+++ parallel.go
@@ -1,16 +1,36 @@
 package main
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//#include <time.h>
+import "C"
+
 import (
-    "fmt"
-    "log"
-    "time"
-    "math/rand"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
 )
 
-func main() {
-    log.Print("started.")
+var global int = 123
 
-    rand.Seed(time.Now().UnixNano())
-    fmt.Println("My favorite number is", rand.Intn(10))
-    log.Print("end.")
+func test(number int) **int{
+  return number
+}
+
+func main() {
+	log.Print("started.")
+	var name int = 1000
+	var numberOfCities int = 51
+  var pointerTest *int = &name
+  var doublePointerTest **int = &pointerTest
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("My favorite number is", rand.Intn(10))
+  fmt.Println("int:", name,numberOfCities)
+  fmt.Println("pointer:", pointerTest)
+  fmt.Println("doublePointer:", *doublePointerTest)
+  fmt.Println("functionPointer:", test(name))
+	C.main()
+	log.Print("end.")
 }
